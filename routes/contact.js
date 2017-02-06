699efd6d024aa4374941439dc08c8205a8d51c75
AfredVon <shanejinru@gmail.com>
2017-02-06 09:04:50 +0800

    Add email validation

--- routes/contact.js
+++ routes/contact.js
@@ -9,12 +9,38 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.post('/', function(req, res, next) {
-    console.log('Name: ' + req.body.name);
-    console.log('Email: ' + req.body.email);
-    console.log('Message: ' + req.body.messages);
+// 检验必填项
+function isFilled(field) {
+    return field !== undefined && field !== '';
+}
 
-    res.send('Form posted successfully');
+// 检验email地址
+var emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function isValidEmail(email) {
+    return emailRegex.test(email);
+}
+
+router.post('/', function(req, res, next) {
+    var response = 'Form posted successfully',
+        required = [ 'name', 'email', 'message' ],
+        missing = [];
+
+    //检查必填项
+    missing = required.filter(function(prop) {
+        return !isFilled(req.body[prop]);
+    });
+
+    if ( missing.length ) {
+        response = 'Please fill out all required fields (' + missing.join(', ') + ')';
+    }
+
+    //检查email
+    else if ( !isValidEmail( req.body.email ) ) {
+        response = 'Please enter a valid email address';
+    }
+
+    res.send( response );
 });
 
 module.exports = router;
